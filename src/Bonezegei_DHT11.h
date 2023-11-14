35c41957a13f90e0138584e0a05e8bd7e84d231e
Bonezegei <93502321+bonezegei@users.noreply.github.com>
2023-11-15 07:33:06 +0800

    Update Bonezegei_DHT11.h

--- src/Bonezegei_DHT11.h
+++ src/Bonezegei_DHT11.h
@@ -4,6 +4,21 @@
   Date : 
 */
 
-class Bonezegei_DHT11{
-  public:
+#include <Arduino.h>
+
+#ifndef _BONEZEGEI_DHT11_H_
+#define _BONEZEGEI_DHT11_H_
+
+class Bonezegei_DHT11 {
+public:
+  Bonezegei_DHT11();
+  Bonezegei_DHT11(int8_t pin);
+  char begin();
+  char getData();
+  float getTemperature();
+  float getHumidity();
+
+  int8_t _pin;
+  uint8_t data[6];
+  unsigned long prevtime;
 }
