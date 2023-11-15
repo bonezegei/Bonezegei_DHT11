b1d4a05470dc585926f96dc7c0380498696c9c72
Bonezegei <93502321+bonezegei@users.noreply.github.com>
2023-11-15 18:23:43 +0800

    Update Bonezegei_DHT11.cpp

--- src/Bonezegei_DHT11.cpp
+++ src/Bonezegei_DHT11.cpp
@@ -19,13 +19,53 @@ char Bonezegei_DHT11::begin() {
     return 0;
   } else {
     prevtime = 0;
+    pinMode(_pin, INPUT);
+    digitalWrite(_pin, HIGH);
     return 1;
   }
 }
 
 char Bonezegei_DHT11::getData() {
+  //uint8_t prevstate = HIGH;
+  int cnt = 0;
+  //unsigned long ctime;
+
   data[0] = data[1] = data[2] = data[3] = data[4] = 0;
 
+  digitalWrite(_pin, HIGH);
+  delay(250);
+
+  // now pull it low for ~20 milliseconds
+  pinMode(_pin, OUTPUT);
+  digitalWrite(_pin, LOW);
+  delay(20);
+  noInterrupts();
+  digitalWrite(_pin, HIGH);
+  delayMicroseconds(40);
+  pinMode(_pin, INPUT);
+
+  if (digitalRead(_pin) == LOW) {
+    delayMicroseconds(80);
+    if (digitalRead(_pin) == HIGH) {
+      delayMicroseconds(80);
+
+      for (int b = 0; b < 5; b++) {
+        for (int a = 0; a < 8; a++) {
+          while (digitalRead(_pin) == LOW)
+            ;
+          delayMicroseconds(30);
+          if (digitalRead(_pin) == HIGH) {
+            data[b] |= (1 << (7 - a));
+          }
+          while (digitalRead(_pin) == HIGH)
+            ;
+        }
+      }
+    }
+  }
+ 
+
+  interrupts();
 
   return 0;
 }
