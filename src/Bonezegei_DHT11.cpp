aec14105775f5e9e359f363555d221e8e8215ef2
Bonezegei <93502321+bonezegei@users.noreply.github.com>
2023-11-15 16:18:03 +0800

    Update Bonezegei_DHT11.cpp

--- src/Bonezegei_DHT11.cpp
+++ src/Bonezegei_DHT11.cpp
@@ -5,3 +5,36 @@
 */
 
 #include "Bonezegei_DHT11.h"
+
+Bonezegei_DHT11::Bonezegei_DHT11() {
+  _pin = -1;
+}
+
+Bonezegei_DHT11::Bonezegei_DHT11(int8_t pin) {
+  _pin = pin;
+}
+
+char Bonezegei_DHT11::begin() {
+  if (_pin < 0) {
+    return 0;
+  } else {
+    prevtime = 0;
+    return 1;
+  }
+}
+
+char Bonezegei_DHT11::getData() {
+  data[0] = data[1] = data[2] = data[3] = data[4] = 0;
+
+
+  return 0;
+}
+
+float Bonezegei_DHT11::getTemperature() {
+
+  return 0;
+}
+
+float Bonezegei_DHT11::getHumidity() {
+  return 0;
+}
